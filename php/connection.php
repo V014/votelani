17d2deea18ff87069f6ed5286320a1e4dd961430
Wanga Kanjala <wangakanjala@gmail.com>
2024-04-17 22:16:06 +0200

    working on db now

--- php/connection.php
+++ php/connection.php
@@ -5,6 +5,7 @@ $password = "";
 $database = "votelani";
     try{
         $connection = mysqli_connect($host, $username, $password, $database);
+        echo "<script>alert('Database online');</script>";
     }
     catch(exception){
         echo "<script>alert('An error occurred, please try again later');</script>";
