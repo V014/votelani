18ccfe695b5737998ce53e68fccf5a306f998d5a
Wanga Kanjala <wangakanjala@gmail.com>
2024-04-17 12:50:17 +0200

    added exception for unavailable database

--- php/connection.php
+++ php/connection.php
@@ -3,6 +3,11 @@ $host = "localhost";
 $username = "root";
 $password = "";
 $database = "votelani";
-$connection = mysqli_connect($host, $username, $password, $database) 
-or die ("We're experiencing a problem, please try again later");
+    try{
+        $connection = mysqli_connect($host, $username, $password, $database) 
+        or die ("We're experiencing a problem, please try again later");
+    }
+    catch(exception){
+        echo "<script>alert('An error occurred, please try again later');</script>";
+    }
 ?>
