0e9a72229776a3447546ca84d8e20b55ba91fe7d
Wanga Kanjala <wangakanjala@gmail.com>
2024-04-20 11:04:16 +0200

    wrote demo code for pulling data from db with the help of Meta AI

--- php/getData.php
+++ php/getData.php
@@ -3,4 +3,18 @@ include connection.php;
 
 // get the selected value from the Get request
 $selectedValue = $_GET['selectedValue'];
+
+ // Query the database
+ $query = "SELECT COUNT(ID) FROM `event` WHERE myColumn = '$selectedValue'";
+ $result = mysqli_query($connection, $query);
+
+ // Fetch the data
+ $data = '';
+ while ($row = mysqli_fetch_assoc($result)) {
+    $data .= '<p>' . $row['myField'] . '</p>';
+ }
+
+ // Close the connection
+ mysqli_close($connection);
+ echo $data;
 ?>
