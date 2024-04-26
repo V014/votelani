8198d2611baa8c32d914204a6d1a81c1e239169c
Wanga Kanjala <wangakanjala@gmail.com>
2024-04-26 10:54:22 +0200

    reflected the conn name to the connection name as it seems most programmers use it

--- php/connection.php
+++ php/connection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $database = "votelani";
     try{
-        $connection = mysqli_connect($host, $username, $password, $database);
+        $conn = mysqli_connect($host, $username, $password, $database);
     }
     catch(exception){
         echo "<script>alert('An error occurred, please try again later');</script>";
