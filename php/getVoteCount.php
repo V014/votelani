c8894f2e09ee323ace8b149c61e14eb74f4dc296
Wanga Kanjala <wangakanjala@gmail.com>
2024-05-11 18:11:42 +0200

    removed useless block of code

--- php/getVoteCount.php
+++ php/getVoteCount.php
@@ -6,12 +6,6 @@ $selectedCandidate = $_GET['selectedCandidate'];
 $selectionOption = $_GET['selectionOption'];
 $candidateID = $selectedCandidate;
 
-// Retrieve the CandidateID from the votecounts table
-// $query = "SELECT CandidateID FROM votecounts WHERE EventID = '$selectionOption' AND CandidateID = '$selectedCandidate'";
-// $result = mysqli_query($conn, $query);
-// $row = mysqli_fetch_array($result);
-// $candidateID = $row['CandidateID'];
-
 // Retrieve the vote count from the votes table
 $query = "SELECT COUNT($candidateID) as vote_count FROM votes WHERE CandidateID = '$candidateID'";
 $result = mysqli_query($conn, $query);
