8f82baa2e6bb09b6aff96b78a8f2a03f16c462a4
Wanga Kanjala <wangakanjala@gmail.com>
2024-05-31 10:35:35 +0200

    modifying js code to identify data-id value

--- js/castVote.js
+++ js/castVote.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
     $("#submit").click(function(e){
         e.preventDefault(); // Prevent the form from submitting normally
-      var president = $('#president-select').val();
-    });
+      
+        // Get the form data
+        var voteID = $("input[name='voteID']").data('id');
+        var president = $("select[name='president'] option:selected").data(id);
+        var chancellor = $("select[name='chancellor'] option:selected").data(id);
+        var mp = $("select[name='mp'] option:selected").data(id);
 
-    $.ajax({
-      url:'php/castVote.php',
-      data:'Vote casted!',
-      success:function(data){
-        $('#content').html(data);
-      }
-    })  
+        // Send the data to submit_vote.php using Ajax
+        $.ajax({
+        type: "POST",
+        url: "../php/castVote.php",
+        data: {
+            voteID: voteID,
+            president: president,
+            chancellor: chancellor,
+            mp: mp
+        },
+        success: function(data){
+          $("#reply").html(data); // Show the response from submit_vote.php
+        }
+      });
+    }); 
   });
