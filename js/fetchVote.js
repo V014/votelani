f5908d87dff22cc4590302ecb540de6c6505afb1
Wanga Kanjala <wangakanjala@gmail.com>
2024-06-02 13:07:10 +0200

    Finally able to pull data for candidate vote counts with new votes schema

--- js/fetchVote.js
+++ js/fetchVote.js
@@ -20,6 +20,7 @@ function fetchVote(selectElement, voteTarget) {
         fetch(`php/getVoteCount.php?selectedCandidate=${candidateID}&selectionOption=${candidateType}`)
             .then(response => {
             response.json().then(json => voteTarget.innerHTML = json['vote_count']);
+            debugger
             })
             .catch(error => {
             console.error('Error fetching vote count:', error);
--- php/getVoteCount.php
+++ php/getVoteCount.php
@@ -6,14 +6,14 @@ $selectedCandidate = $_GET['selectedCandidate'];
 $selectionOption = $_GET['selectionOption'];
 
 // Retrieve the vote count from the votes table
-$query = "SELECT COUNT($selectedCandidate) as vote_count FROM votes'";
+$query = "SELECT COUNT($selectionOption) as vote_count FROM votes WHERE $selectionOption = $selectedCandidate";
 $result = mysqli_query($conn, $query);
 $row = mysqli_fetch_array($result);
 $voteCount = $row['vote_count'];
 
 // Return the vote count in JSON format
 // echo json_encode(['vote_count' => $voteCount]);
-echo json_encode(['vote_count' => $selectionOption]);
+echo json_encode(['vote_count' => $voteCount]);
 
 // Close the database connection
 mysqli_close($conn);
