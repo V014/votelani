ebfe139cad24a7f771ab326f50383086528ffd09
Wanga Kanjala <wangakanjala@gmail.com>
2024-06-02 12:12:46 +0200

    rewired code to receive data to query database

--- js/fetchVote.js
+++ js/fetchVote.js
@@ -20,7 +20,6 @@ function fetchVote(selectElement, voteTarget) {
         fetch(`php/getVoteCount.php?selectedCandidate=${candidateID}&selectionOption=${candidateType}`)
             .then(response => {
             response.json().then(json => voteTarget.innerHTML = json['vote_count']);
-            debugger
             })
             .catch(error => {
             console.error('Error fetching vote count:', error);
--- php/getVoteCount.php
+++ php/getVoteCount.php
@@ -4,15 +4,12 @@ include 'connection.php';
 // Get the selected candidate and selection option from the GET request
 $selectedCandidate = $_GET['selectedCandidate'];
 $selectionOption = $_GET['selectionOption'];
-$candidateID = $selectedCandidate;
 
 // Retrieve the vote count from the votes table
-/*
-$query = "SELECT COUNT($candidateID) as vote_count FROM votes WHERE CandidateID = '$candidateID'";
+$query = "SELECT COUNT($selectedCandidate) as vote_count FROM votes'";
 $result = mysqli_query($conn, $query);
 $row = mysqli_fetch_array($result);
 $voteCount = $row['vote_count'];
-*/
 
 // Return the vote count in JSON format
 // echo json_encode(['vote_count' => $voteCount]);
