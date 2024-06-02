f8527276932bc7a2c4ed28aad6f1cdf415fbb29e
Wanga Kanjala <wangakanjala@gmail.com>
2024-06-02 11:43:31 +0200

    changed file locations for fetch vote function

--- index.php
+++ index.php
@@ -111,7 +111,6 @@ session_start();
         </p>
       </div>
     </div>
-
   </div> <!-- end of content -->
 
   <footer>
@@ -119,45 +118,7 @@ session_start();
   </footer>
   <script src="js/changeImage.js"></script>
   <script src="js/castVote.js"></script>
-  <script>
-    // function that checks the current vote count of selected candidate
-    function fetchVote(selectElement, voteTarget) {
-      //Add onchange event listener to the selectElement and when its
-      //selected value changes trigger a fetch request to the getVoteCount.php
-      //endpoint and parse the JSON response vote count and update the value
-      //of the voteTarget element
-      selectElement = document.getElementById(selectElement);
-      voteTarget = document.getElementById(voteTarget);
-
-      selectElement.addEventListener("change", function(e) {
-        //Access the available options in the select element and get the one
-        //option the user has chosen using the currently selected index of the
-        //select element
-        let selectedOption = selectElement.options[selectElement.selectedIndex];
-        let candidateID = selectedOption.getAttribute('data-id');
-        let candidateType = selectElement.getAttribute('id');
-
-        if (candidateID != null || candidateID != undefined) {
-          // Make an AJAX request to retrieve the vote count
-          fetch(`php/getVoteCount.php?selectedCandidate=${candidateID}&selectionOption=${candidateType}`)
-            .then(response => {
-              response.json().then(json => voteTarget.innerHTML = json['vote_count']);
-              debugger
-            })
-            .catch(error => {
-              console.error('Error fetching vote count:', error);
-              voteTarget.innerHTML = 'Error fetching vote count';
-            });
-        } else {
-          voteTarget.innerHTML = 0;
-        }
-      });
-    }
-
-    fetchVote("PresidentID", "president-vote-count");
-    fetchVote("ChancellorID", "chancellor-vote-count");
-    fetchVote("MPID", "mp-vote-count");
-  </script>
+  <script src="js/fetchVote.js"></script>
 </body>
 
 </html>
