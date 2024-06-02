1c42acdcd4008519d2aa3c27ea996e54ed10311d
Wanga Kanjala <wangakanjala@gmail.com>
2024-06-02 15:50:31 +0200

    made backup of new schema

--- db/votelani.sql
+++ db/votelani.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 31, 2024 at 10:48 PM
+-- Generation Time: Jun 02, 2024 at 03:49 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
